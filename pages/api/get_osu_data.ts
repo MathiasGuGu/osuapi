75154fb3cbb5e40245b88f3138128cd6c7df9c52
Magugu <265058@uis.no>
2022-12-11 01:30:07 +0100

    asd

--- pages/api/get_osu_data.ts
+++
@@ -1,9 +0,0 @@
-
-import {data} from "../../osuApiData"
-import { get_token } from "./get_token"
-const headers = {
-    Authorization: `bearer: ${get_token()}`
-  }
-const get_osu_data = async () => {
-    const response = await fetch(data.BASE_URL + "/endpoint", {method: "GET", headers: headers})
-}
--- pages/api/get_token.ts
+++
@@ -1,8 +0,0 @@
-import {data} from "../../osuApiData"
-
-
-
-export const get_token = async () => {
-    const response = await fetch(data.TOKEN_URL)
-    return response.json
-  }
